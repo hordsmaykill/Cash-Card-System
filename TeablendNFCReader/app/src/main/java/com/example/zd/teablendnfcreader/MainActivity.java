0c82fbe104ad1d281b666c99f72f77e0ef2da668
Zaerald Denze Lungos <zaerald.zd@gmail.com>
2016-09-20 06:13:10 +0800

    NDEF Discovere Intent

--- TeablendNFCReader/app/src/main/java/com/example/zd/teablendnfcreader/MainActivity.java
+++ TeablendNFCReader/app/src/main/java/com/example/zd/teablendnfcreader/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.zd.teablendnfcreader;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "NFC";
+
+    private TextView mTextView;
+    private NfcAdapter mNfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.textViewExplanation);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (mNfcAdapter == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+
+        }
+
+        if (!mNfcAdapter.isEnabled()) {
+            mTextView.setText("NFC is disabled.");
+        } else {
+            mTextView.setText(R.string.explanation);
+        }
+
+        handleIntent(getIntent());
+    }
+
+    private void handleIntent(Intent intent) {
+        // TODO: handle Intent
     }
 }
