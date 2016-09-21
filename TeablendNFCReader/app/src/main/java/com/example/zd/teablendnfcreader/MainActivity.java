e375157543e459ddaf09ee81684a65d1c8a431a8
Zaerald Denze Lungos <zaerald.zd@gmail.com>
2016-09-22 07:15:37 +0800

    update android codes

--- TeablendNFCReader/app/src/main/java/com/example/zd/teablendnfcreader/MainActivity.java
+++ TeablendNFCReader/app/src/main/java/com/example/zd/teablendnfcreader/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.zd.teablendnfcreader;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.app.PendingIntent;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.nfc.NdefMessage;
@@ -13,10 +15,22 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,14 +41,18 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTextView;
     private NfcAdapter mNfcAdapter;
 
+    EditText txtIp;
+    private String ip;
+    private String cusNo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.textViewExplanation);
-
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        txtIp = (EditText) findViewById(R.id.txtIp);
 
         if (mNfcAdapter == null) {
             // Stop here, we definitely need NFC
@@ -46,11 +64,11 @@ public class MainActivity extends AppCompatActivity {
 
         if (!mNfcAdapter.isEnabled()) {
             mTextView.setText("NFC is disabled.");
-        } else {
-            mTextView.setText(R.string.explanation);
         }
-
         handleIntent(getIntent());
+
+        // nfc is present
+        // update cus_no
     }
 
     @Override
@@ -97,6 +115,13 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    public void onClickUpdate(View v) {
+        ip = txtIp.getText().toString();
+        txtIp.setText("");
+        txtIp.setHint("IP: " + ip);
+        Toast.makeText(getApplicationContext(), "IP Updated", Toast.LENGTH_SHORT).show();
     }
 
 
@@ -191,11 +216,100 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String result) {
             if (result != null) {
-                mTextView.setText("Read content: " + result);
+                cusNo = result;
+                new BGWorker().execute(cusNo);
             }
         }
 
     }
 
+    private class BGWorker extends AsyncTask<String, Void, String>{
+        Dialog mLoadingDialog;
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            //String loginUrl = "http://" + ip + "/tbc/update.php";
+            String loginUrl = "http://192.168.22.10/tbc/update.php";
+
+            try {
+                String cusNo = strings[0];
+
+                URL url = new URL(loginUrl);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setConnectTimeout(3000);
+
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.setDoOutput(true);
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(
+                        new OutputStreamWriter(outputStream, "UTF-8"));
+
+                String postData =
+                        URLEncoder.encode("cusNo", "UTF-8") + "=" +
+                                URLEncoder.encode(cusNo, "UTF-8");
+
+                bufferedWriter.write(postData);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(
+                        new InputStreamReader(inputStream, cusNo));
+
+                String result = "";
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    result += line;
+                }
+
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+
+                Log.i("NFO", "no err");
+
+                return result;
+
+            } catch(IOException e) {
+                Log.e("ERR", "Error in login: " + e.getMessage());
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            //super.onPreExecute();
+            mLoadingDialog = ProgressDialog.show(MainActivity.this, "Please wait", "Loading...");
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            //super.onPostExecute(s);
+            mLoadingDialog.dismiss();
+
+            // check if connected
+            if (s == null) {
+                Toast.makeText(getApplicationContext(), "Please connect to the internet. " + ip, Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            String out = s.trim();
+
+            if(!s.equals("fail")) {
+                // show dialog success
+                Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Invalid Card!\nPlease contact Teablend", Toast.LENGTH_SHORT).show();
+            }
+
+            Log.i("NFO", "Login NFO: " + out);
+
+        }
+    }
 
 }
