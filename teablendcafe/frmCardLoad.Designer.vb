818522c0f1febc0c931c4c4c06de79df315615ba
hordsmaykill <jmalejo800@gmail.com>
2016-09-22 05:44:13 +0800

    Reading transaction is now ok

--- teablendcafe/frmCardLoad.Designer.vb
+++ teablendcafe/frmCardLoad.Designer.vb
@@ -22,29 +22,62 @@ Partial Class frmCardLoad
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.btnClose = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.tmrCheck = New System.Windows.Forms.Timer(Me.components)
         Me.SuspendLayout()
         '
         'btnClose
         '
-        Me.btnClose.Location = New System.Drawing.Point(456, 267)
+        Me.btnClose.Location = New System.Drawing.Point(448, 268)
         Me.btnClose.Name = "btnClose"
         Me.btnClose.Size = New System.Drawing.Size(97, 40)
         Me.btnClose.TabIndex = 0
         Me.btnClose.Text = "Cancel"
         Me.btnClose.UseVisualStyleBackColor = True
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(250, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Transacting"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(236, 121)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(132, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Rotating/Loading Gif Here"
+        '
+        'tmrCheck
+        '
+        Me.tmrCheck.Enabled = True
+        Me.tmrCheck.Interval = 500
+        '
         'frmCardLoad
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(587, 338)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.btnClose)
         Me.Name = "frmCardLoad"
         Me.Text = "frmCardLoad"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents btnClose As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents tmrCheck As Timer
 End Class
--- teablendcafe/frmCardLoad.vb
+++ teablendcafe/frmCardLoad.vb
@@ -6,30 +6,65 @@ Imports MySql.Data.MySqlClient
 
 Public Class frmCardLoad
 
-    Dim customerId As String = ""
+    Public customerId As String = "tbc123"
+
+    Dim Connect As New MySqlConnection
+    Dim Reader As MySqlDataReader
 
     Private Sub frmCardLoad_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        ConnectDB()
     End Sub
 
     Public Sub ConnectDB()
-        Dim Command As New MySqlCommand
-        Dim Reader As MySqlDataReader
-        Dim Connect As New MySqlConnection
-        Dim str As String
 
+        Dim str As String
         If Connect.State = ConnectionState.Closed Then
             str = "server=localhost; userid=root; password=; database=dbtbc; Allow Zero Datetime=True;"
             Connect.ConnectionString = str
             Connect.Open()
-
         End If
 
     End Sub
 
+    Private Sub closeTransaction()
+        Reader.Close()
+        tmrCheck.Enabled = False
+
+        Dim Command As New MySqlCommand
+
+        With Command
+            .Connection = Connect
+            .CommandText = "UPDATE tblcustomers SET isTransacting='false' WHERE cus_no='" & customerId & "'"
+            .ExecuteNonQuery()
+        End With
+    End Sub
 
     Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
-
+        closeTransaction()
         Me.Close()
+    End Sub
+
+    Private Sub tmrCheck_Tick(sender As Object, e As EventArgs) Handles tmrCheck.Tick
+        ' checks if isTransaction is true
+
+        Dim Command As New MySqlCommand
+
+            With Command
+                .Connection = Connect
+                .CommandText = "SELECT isTransacting FROM tblcustomers WHERE cus_no='" & customerId & "'"
+            End With
+            Reader = Command.ExecuteReader()
+            Reader.Read()
+
+        If Reader.Item(0) = "true" Then
+            tmrCheck.Enabled = False
+            Reader.Close()
+            MsgBox("NAPALITAN NA!!! BALIK KNA SA MAIN BES.")
+            Me.Close()
+        End If
+
+
+
+        Reader.Close()
     End Sub
 End Class
