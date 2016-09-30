d6d960ade090a28b6e12ec6a64bf2f8bd086f5b5
hordsmaykill <jmalejo800@gmail.com>
2016-10-01 00:40:17 +0800

    commit after error

---
+++ teablendcafe/frmetramount.vb
@@ -0,0 +1,9 @@
+﻿Public Class frmetramount
+    Private Sub btncancel_Click(sender As Object, e As EventArgs) Handles btncancel.Click
+        Me.Dispose()
+    End Sub
+
+    Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+        frmtotal.ShowDialog()
+    End Sub
+End Class
---
+++ teablendcafe/frmtotal.vb
@@ -0,0 +1,94 @@
+﻿
+Imports MySql.Data.MySqlClient
+Public Class frmtotal
+
+    Dim Command As New MySqlCommand
+    Dim Reader As MySqlDataReader
+    Dim Connect As New MySqlConnection
+    Dim str As String
+
+    Dim tendered As Double
+    Dim totalamount As Double
+    Dim change As Double
+
+    Public Sub ConnectDB()
+
+        Dim str As String
+        If Connect.State = ConnectionState.Closed Then
+            str = "server=localhost; userid=root; password=; database=dbtbc; Allow Zero Datetime=True;"
+            Connect.ConnectionString = str
+            Connect.Open()
+        End If
+
+    End Sub
+
+
+    Private Sub frmtotal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call ConnectDB()
+
+
+
+        tendered = Double.Parse(frmetramount.tbenterpayment.Text)
+        paymenttendered.Text = tendered
+        change = Double.Parse(tbchangee.Text)
+        totalamount = Double.Parse(tbtotal.Text)
+
+        change = tendered - totalamount
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim reader As MySqlDataReader
+        Dim cmd As New MySqlCommand
+        cmd.Connection = Connect
+
+        Dim total As Double = frmMain.txtTotalOrder.Text
+        ' update products and inventories
+        Dim curDate As String = Date.Today.ToString("yyyy-MM-dd")
+
+        ' generate random
+        Randomize()
+        Dim rand As Integer = CInt(Int((99999 * Rnd()))) + 1
+        Dim dateConcat As String = (DateTime.Now()).ToString("ddMMyyhhmmss")
+        Dim ord_code As String = dateConcat & "-" & rand
+
+
+        ' get data
+
+
+        ' insert id and date
+        cmd.CommandText = "INSERT INTO tblorders VALUES('" & ord_code & "', " & total & ",'" & change & "','" & paymenttendered.Text & "','" & curDate & "')"
+        cmd.ExecuteNonQuery()
+
+        For i As Integer = 0 To frmMain.dgvorders.RowCount - 1
+            Dim prodCode As String = frmMain.dgvorders.Item(0, i).Value
+            Dim prodName As String = frmMain.dgvorders.Item(1, i).Value
+            Dim qty As Integer = frmMain.dgvorders.Item(2, i).Value
+            Dim price As Integer = frmMain.dgvorders.Item(3, i).Value
+
+            ' insert all product codes with qty
+            cmd.CommandText = "INSERT INTO tblorder_prod VALUES('" & ord_code & "', '" & prodCode & "', '" & prodName & "', " & qty & ", " & price & ")"
+            cmd.ExecuteNonQuery()
+
+            ' decrement a qty in tblinventory
+            With cmd
+                .Connection = Connect
+                .CommandText = "SELECT inv_qty FROM tblinventory WHERE inv_prod_code='" & prodCode & "'"
+            End With
+
+
+            Dim qtyFromDB As Integer
+            reader = cmd.ExecuteReader()
+
+            reader.Read()
+            If reader.HasRows Then
+                qtyFromDB = reader.Item(0)
+            End If
+            reader.Close()
+
+            Dim qtyVal As Integer = qtyFromDB - qty
+            cmd.CommandText = "UPDATE tblinventory SET inv_qty=" & qtyVal & " WHERE inv_prod_code='" & prodCode & "'"
+            cmd.ExecuteNonQuery()
+        Next
+        MsgBox("Success!")
+    End Sub
+End Class
