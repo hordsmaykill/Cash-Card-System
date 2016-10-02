67a9397210924ffffdbd3c105eb998a9e515522d
Zaerald Denze Lungos <zaerald.zd@gmail.com>
2016-10-03 02:56:13 +0800

    added clear datagridview after proceed for issue #40

--- teablendcafe/frmremainingload.vb
+++ teablendcafe/frmremainingload.vb
@@ -1,6 +1,7 @@
 ﻿Public Class frmremainingload
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        frmMain.dgvorders.Rows.Clear()
         Me.Close()
     End Sub
 End Class
